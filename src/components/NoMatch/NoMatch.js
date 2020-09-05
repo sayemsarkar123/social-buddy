3d91f6ba299d81b7fd65f1eb8d7be6ddcd118a7f
sayemsarkar123 <sayemsarkar123@gmail.com>
2020-09-05 21:33:17 +0600

    added 404 content

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NoMatch.css';
 
 const NoMatch = () => {
   return (
-    <div>
-
+    <div style={{height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <h1>404</h1>
+      <h3>The page you're looking <br/> for can't be found</h3>
     </div>
   );
 };
